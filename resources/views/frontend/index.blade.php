8463a9f250ddacadd5b80c3697077b4e0e5c77c3
sazzadursiam <sazzadur.swe@gmail.com>
2024-02-02 01:47:25 +0600

    theme change

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,89 +4,137 @@
 @endsection
 @section('custom_css')
     <style>
-        .basic_btn {
-            border: 1px solid #07d5c0;
-            color: #07d5c0;
-        }
 
-        .basic_btn:hover {
-            background: #07d5c0;
-            color: #ffffff;
-        }
-
-        .starter_btn {
-            border: 1px solid #65c600;
-            color: #65c600;
-        }
-
-        .starter_btn:hover {
-            background: #65c600;
-            color: #ffffff;
-        }
-
-        .business_btn {
-            border: 1px solid #ff901c;
-            color: #ff901c;
-        }
-
-        .business_btn:hover {
-            background: #ff901c;
-            color: #ffffff;
-        }
-
-        .ultimate_btn {
-            border: 1px solid #ff0071;
-            color: #ff0071;
-        }
-
-        .ultimate_btn:hover {
-            background: #ff0071;
-            color: #ffffff;
-        }
-
-        .hero .btn-get-started:hover {
-            margin-top: 30px;
-            line-height: 0;
-            padding: 15px 40px;
-            border-radius: 4px;
-            transition: 0.5s;
-            color: #fff;
-            background: #012970;
-            box-shadow: 0px 5px 30px rgba(65, 84, 241, 0.4);
-        }
-
-        /* .cb1:hover {
-                            background: #717ef3;
-                        }
-
-                        .cb1:hover i,
-                        .cb1:hover p,
-                        .cb1:hover span {
-                            color: #ffffff;
-                        } */
     </style>
 @endsection
 
 @section('content')
     <!-- ======= About Section ======= -->
-    @include('frontend._partials.about')
+    {{-- @include('frontend._partials.about') --}}
     <!-- End About Section -->
 
-    <!-- ======= Pricing Section ======= -->
-    @include('frontend._partials.pricing')
-    <!-- End Pricing Section -->
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container">
+            <h1>Authentic Trustpilot Reviews for Credibility!</h1>
+            <h2>Unlock Success: Your Path to Trustworthy Reviews Starts Here!</h2>
+            {{-- <a href="#about" class="btn-get-started scrollto">Get Started</a> --}}
+        </div>
+    </section><!-- End Hero -->
 
-    <!-- ======= Counts Section ======= -->
-    @include('frontend._partials.counts')
-    <!-- End Counts Section -->
+    <main id="main">
+
+        <!-- ======= Why Us Section ======= -->
+        <section id="why-us" class="why-us">
+            <div class="container">
+
+                <div class="row">
+                    {{-- <div class="col-lg-4 d-flex align-items-stretch">
+                        <div class="content">
+                            <h3>Why Choose Medilab?</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus
+                                optio ad corporis.
+                            </p>
+                            <div class="text-center">
+                                <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
+                            </div>
+                        </div>
+                    </div> --}}
+                    <div class="col-lg-12 d-flex align-items-stretch">
+                        <div class="icon-boxes d-flex flex-column justify-content-center">
+                            <div class="row">
+                                <div class="col-xl-4 d-flex align-items-stretch">
+                                    <div class="icon-box mt-4 mt-xl-0">
+                                        <i class="bx bx-receipt"></i>
+                                        <h4>Corporis voluptates sit</h4>
+                                        <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut
+                                            aliquip</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 d-flex align-items-stretch">
+                                    <div class="icon-box mt-4 mt-xl-0">
+                                        <i class="bx bx-cube-alt"></i>
+                                        <h4>Ullamco laboris ladore pan</h4>
+                                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                            deserunt</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 d-flex align-items-stretch">
+                                    <div class="icon-box mt-4 mt-xl-0">
+                                        <i class="bx bx-images"></i>
+                                        <h4>Labore consequatur</h4>
+                                        <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis
+                                            facere</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- End .content-->
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <!-- End Why Us Section -->
+
+
+        <!-- ======= Counts Section ======= -->
+        {{-- <section id="counts" class="counts">
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="count-box">
+                            <i class="fas fa-user-md"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="85" data-purecounter-duration="1"
+                                class="purecounter"></span>
+                            <p>Doctors</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
+                        <div class="count-box">
+                            <i class="far fa-hospital"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="1"
+                                class="purecounter"></span>
+                            <p>Departments</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                        <div class="count-box">
+                            <i class="fas fa-flask"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="12" data-purecounter-duration="1"
+                                class="purecounter"></span>
+                            <p>Research Labs</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                        <div class="count-box">
+                            <i class="fas fa-award"></i>
+                            <span data-purecounter-start="0" data-purecounter-end="150" data-purecounter-duration="1"
+                                class="purecounter"></span>
+                            <p>Awards</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section> --}}
+        <!-- End Counts Section -->
 
 
 
-    <!-- ======= Services Section ======= -->
-    {{-- @include('frontend._partials.services') --}}
-    <!-- End Services Section -->
+        <!-- ======= Frequently Asked Questions Section ======= -->
 
-    <!-- ======= Contact Section ======= -->
-    @include('frontend._partials.contact')
-    <!-- End Contact Section -->
+        @include('frontend._partials.faq')
+
+        <!-- End Frequently Asked Questions Section -->
+
+    </main><!-- End #main -->
 @endsection
